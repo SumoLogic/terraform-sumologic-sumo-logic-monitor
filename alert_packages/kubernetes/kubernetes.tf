ba4f693f119e958e00992d0113cf3f6aebafac0d
arunpatyal <arun@sumologic.com>
2021-01-15 16:13:38 +0530

    Initial Commit.

--- alert_packages/kubernetes/kubernetes.tf
+++ alert_packages/kubernetes/kubernetes.tf
@@ -353,3 +353,87 @@ module "ClusterCPUUtilizationHigh" {
   connection_notifications  = var.connection_notifications
   email_notifications       = var.email_notifications
 }
+
+module "PrometheusRemoteStorageFailures" {
+  source                    = "SumoLogic/sumo-logic-monitor/sumologic"
+  #version                  = "{revision}"
+  alert_name                = "PrometheusRemoteStorageFailures"
+  alert_description         = "Prometheus fails to send samples to remote storage."
+  alert_monitor_type        = "Metrics"
+  alert_parent_id           = sumologic_monitor_folder.tf_monitor_folder_1.id
+
+  # Queries - Multiple queries allowed for Metrics monitor
+  queries = {
+    A = "metric=prometheus_remote_storage_failed_samples_total (job=collection-kube-prometheus-prometheus  or job=collection-prometheus-oper-prometheus) | quantize 5m | rate"
+    B = "metric=prometheus_remote_storage_succeeded_samples_total (job=collection-kube-prometheus-prometheus  or job=collection-prometheus-oper-prometheus) | quantize 5m | rate"
+    C = "100*(#A/(#A+#B)) along namespace,pod"
+  }
+
+  # Triggers
+  triggers = [
+              {
+                  threshold_type = "GreaterThan",
+                  threshold = 1,
+                  time_range = "5m",
+                  occurrence_type = "Always" # Options: Always, AtLeastOnce and MissingData for Metrics
+                  trigger_source = "AnyTimeSeries" # Options: AllTimeSeries and AnyTimeSeries for Metrics. 'AnyTimeSeries' is the only valid triggerSource for 'Critical' trigger
+                  trigger_type = "Critical",
+                  detection_method = "StaticCondition"
+                },
+                {
+                  threshold_type = "LessThan",
+                  threshold = 1,
+                  time_range = "5m",
+                  occurrence_type = "Always" # Options: Always, AtLeastOnce and MissingData for Metrics
+                  trigger_source = "AnyTimeSeries" # Options: AllTimeSeries and AnyTimeSeries for Metrics. 'AnyTimeSeries' is the only valid triggerSource for 'Critical' trigger
+                  trigger_type = "ResolvedCritical",
+                  detection_method = "StaticCondition"
+                }
+            ]
+
+  # Notifications
+  group_notifications       = var.group_notifications
+  connection_notifications  = var.connection_notifications
+  email_notifications       = var.email_notifications
+}
+
+module "MultipleTerminatedPodsFounds" {
+  source                    = "SumoLogic/sumo-logic-monitor/sumologic"
+  #version                  = "{revision}"
+  alert_name                = "Multiple Terminated pods founds"
+  alert_description         = "Alerts when there are pods that have been terminated."
+  alert_monitor_type        = "Metrics"
+  alert_parent_id           = sumologic_monitor_folder.tf_monitor_folder_1.id
+
+  # Queries - Multiple queries allowed for Metrics monitor
+  queries = {
+    A = "metric=kube_pod_container_status_terminated_reason !reason=completed | sum"
+  }
+
+  # Triggers
+  triggers = [
+              {
+                  threshold_type = "GreaterThan",
+                  threshold = 5,
+                  time_range = "5m",
+                  occurrence_type = "AtLeastOnce" # Options: Always, AtLeastOnce and MissingData for Metrics
+                  trigger_source = "AnyTimeSeries" # Options: AllTimeSeries and AnyTimeSeries for Metrics. 'AnyTimeSeries' is the only valid triggerSource for 'Critical' trigger
+                  trigger_type = "Critical",
+                  detection_method = "StaticCondition"
+                },
+                {
+                  threshold_type = "LessThan",
+                  threshold = 5,
+                  time_range = "5m",
+                  occurrence_type = "Always" # Options: Always, AtLeastOnce and MissingData for Metrics
+                  trigger_source = "AnyTimeSeries" # Options: AllTimeSeries and AnyTimeSeries for Metrics. 'AnyTimeSeries' is the only valid triggerSource for 'Critical' trigger
+                  trigger_type = "ResolvedCritical",
+                  detection_method = "StaticCondition"
+                }
+            ]
+
+  # Notifications
+  group_notifications       = var.group_notifications
+  connection_notifications  = var.connection_notifications
+  email_notifications       = var.email_notifications
+}
