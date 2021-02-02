f050c6a5337a9ea90df8c28ee036b9809f431879
arunpatyal <arun@sumologic.com>
2021-02-02 15:59:15 +0530

    Redis Alerts + K8s Alerts Readme fix.

---
+++ monitor_packages/redis/variables.tf
@@ -0,0 +1,31 @@
+variable "connection_notifications" {
+  type        = list(object(
+                {
+                  connection_type = string,
+                  connection_id = string,
+                  payload_override = string,
+                  run_for_trigger_types = list(string)
+                }
+    ))
+  description = "Connection Notifications to be sent by the alert."
+}
+
+variable "email_notifications" {
+  type        = list(object(
+                {
+                  connection_type = string,
+                  recipients = list(string),
+                  subject = string,
+                  time_zone = string,
+                  message_body = string,
+                  run_for_trigger_types = list(string)
+                }
+    ))
+  description = "Email Notifications to be sent by the alert."
+}
+
+variable "group_notifications" {
+  type        = bool
+  description = "Whether or not to group notifications for individual items that meet the trigger condition. Defaults to true."
+  default     = true
+}
