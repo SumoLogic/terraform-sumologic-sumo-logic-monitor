6006c9adfb66bb926526f42587305161b0c379ac
sourabh <sourabh@sumologic.com>
2021-02-16 17:28:28 +0530

    updating queries for escape character

--- monitor_packages/nginx/nginx.tf
+++ monitor_packages/nginx/nginx.tf
@@ -54,7 +54,7 @@ module "AccessFromHighlyMaliciousSource" {
   monitor_is_disabled         = var.monitors_disabled
 
   queries = {
-    A = "${var.logs_data_source} | json auto maxdepth 1 nodrop | if (isEmpty(log), _raw, log) as nginx_log_message | _sourceHost as Server | parse regex field=nginx_log_message \"(?<ClientIp>\\\\d{1,3}\\\\.\\\\d{1,3}\\\\.\\\\d{1,3}\\\\.\\\\d{1,3})\" | where ClientIp != \"0.0.0.0\" and ClientIp != \"127.0.0.1\" | count as ip_count by ClientIp, Server | lookup type, actor, raw, threatlevel as MaliciousConfidence from sumo://threat/cs on threat=ClientIp  | json field=raw \"labels[*].name\" as LabelName  | replace(LabelName, \"\\/\",\"->\") as LabelName | replace(LabelName, \"\"\",\" \") as LabelName | where type=\"ip_address\" and MaliciousConfidence=\"high\" | if (isEmpty(actor), \"Unassigned\", actor) as Actor | sum (ip_count) as ThreatCount by ClientIp, Server, MaliciousConfidence, Actor, LabelName"
+    A = "${var.logs_data_source} \n| json auto maxdepth 1 nodrop\n| if (isEmpty(log), _raw, log) as nginx_log_message\n| _sourceHost as Server\n| parse regex field=nginx_log_message \"(?<ClientIp>\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3})\"\n| where ClientIp != \"0.0.0.0\" and ClientIp != \"127.0.0.1\"\n| count as ip_count by ClientIp, Server\n| lookup type, actor, raw, threatlevel as MaliciousConfidence from sumo://threat/cs on threat=ClientIp \n| json field=raw \"labels[*].name\" as LabelName \n| replace(LabelName, \"\\\\/\",\"->\") as LabelName\n| replace(LabelName, \"\\\"\",\" \") as LabelName\n| where type=\"ip_address\" and MaliciousConfidence=\"high\"\n| if (isEmpty(actor), \"Unassigned\", actor) as Actor\n| sum (ip_count) as ThreatCount by ClientIp, Server, MaliciousConfidence, Actor, LabelName"
   }
 
   # Triggers
@@ -95,7 +95,7 @@ module "CriticalErrorMessage" {
   monitor_is_disabled         = var.monitors_disabled
 
   queries = {
-    A = "${var.logs_data_source} | json auto maxdepth 1 nodrop | if (isEmpty(log), _raw, log) as nginx_log_message | _sourceHost as Server | parse regex field=nginx_log_message \"\\\\s\\\\[(?<LogLevel>\\\\S+)\\\\]\\\\s\\\\d+#\\\\d+:\\\\s(?:\\\\*\\\\d+\\\\s|)(?<Message>[A-Za-z][^,]+)(?:,|$)\" | where LogLevel in (\"emerg\", \"alert\", \"crit\") | formatDate(_messageTime, \"MMM/dd/yyyy HH:mm:ss:SSS Z\") as MessageDate | count by MessageDate, Server, LogLevel, Message | fields MessageDate, Server, LogLevel, Message"
+    A = "${var.logs_data_source} \n| json auto maxdepth 1 nodrop\n| if (isEmpty(log), _raw, log) as nginx_log_message\n| _sourceHost as Server\n| parse regex field=nginx_log_message \"\\s\\[(?<LogLevel>\\S+)\\]\\s\\d+#\\d+:\\s(?:\\*\\d+\\s|)(?<Message>[A-Za-z][^,]+)(?:,|$)\"\n| where LogLevel in (\"emerg\", \"alert\", \"crit\")\n| formatDate(_messageTime, \"MMM/dd/yyyy HH:mm:ss:SSS Z\") as MessageDate\n| count by MessageDate, Server, LogLevel, Message\n| fields MessageDate, Server, LogLevel, Message"
   }
 
   # Triggers
@@ -136,7 +136,7 @@ module "HighClientError" {
   monitor_is_disabled         = var.monitors_disabled
 
   queries = {
-    A = "${var.logs_data_source} | json auto maxdepth 1 nodrop | if (isEmpty(log), _raw, log) as nginx_log_message | _sourceHost as Server | parse regex field=nginx_log_message \"(?<Method>[A-Z]+)\\\\s(?<URL>\\\\S+)\\\\sHTTP/[\\\\d\\\\.]+\"\\\\s(?<StatusCode>\\\\d+)\\\\s(?<Size>[\\\\d-]+)\\\\s\"(?<Referrer>.*?)\"\\\\s\"(?<UserAgent>.+?)\".*\" | if (StatusCode matches \"4*\", 1, 0) as ServerError | sum(ServerError) as ServerErrors, count as TotalRequests by Server | (ServerErrors/TotalRequests) * 100 as ErrorPercentage | where ErrorPercentage > 5 | fields Server, ErrorPercentage, ServerErrors, TotalRequests"
+    A = "${var.logs_data_source} \n| json auto maxdepth 1 nodrop\n| if (isEmpty(log), _raw, log) as nginx_log_message\n| _sourceHost as Server\n| parse regex field=nginx_log_message \"(?<Method>[A-Z]+)\\s(?<URL>\\S+)\\sHTTP/[\\d\\.]+\\\"\\s(?<StatusCode>\\d+)\\s(?<Size>[\\d-]+)\\s\\\"(?<Referrer>.*?)\\\"\\s\\\"(?<UserAgent>.+?)\\\".*\"\n| if (StatusCode matches \"4*\", 1, 0) as ServerError\n| sum(ServerError) as ServerErrors, count as TotalRequests by Server\n| (ServerErrors/TotalRequests) * 100 as ErrorPercentage\n| where ErrorPercentage > 5\n| fields Server, ErrorPercentage, ServerErrors, TotalRequests"
   }
 
   # Triggers
@@ -178,7 +178,7 @@ module "HighServerError" {
 
   # Queries - Multiple queries allowed for Metrics monitor
   queries = {
-    A = "${var.logs_data_source} | json auto maxdepth 1 nodrop | if (isEmpty(log), _raw, log) as nginx_log_message | _sourceHost as Server | parse regex field=nginx_log_message \"(?<Method>[A-Z]+)\\\\s(?<URL>\\\\S+)\\\\sHTTP/[\\\\d\\\\.]+\"\\\\s(?<StatusCode>\\\\d+)\\\\s(?<Size>[\\\\d-]+)\\\\s\"(?<Referrer>.*?)\"\\\\s\"(?<UserAgent>.+?)\".*\" | if (StatusCode matches \"5*\", 1, 0) as ServerError | sum(ServerError) as ServerErrors, count as TotalRequests by Server | (ServerErrors/TotalRequests) * 100 as ErrorPercentage | where ErrorPercentage > 5 | fields Server, ErrorPercentage, ServerErrors, TotalRequests"
+    A = "${var.logs_data_source} \n| json auto maxdepth 1 nodrop\n| if (isEmpty(log), _raw, log) as nginx_log_message\n| _sourceHost as Server\n| parse regex field=nginx_log_message \"(?<Method>[A-Z]+)\\s(?<URL>\\S+)\\sHTTP/[\\d\\.]+\\\"\\s(?<StatusCode>\\d+)\\s(?<Size>[\\d-]+)\\s\\\"(?<Referrer>.*?)\\\"\\s\\\"(?<UserAgent>.+?)\\\".*\"\n| if (StatusCode matches \"5*\", 1, 0) as ServerError\n| sum(ServerError) as ServerErrors, count as TotalRequests by Server\n| (ServerErrors/TotalRequests) * 100 as ErrorPercentage\n| where ErrorPercentage > 5\n| fields Server, ErrorPercentage, ServerErrors, TotalRequests"
   }
 
   # Triggers
