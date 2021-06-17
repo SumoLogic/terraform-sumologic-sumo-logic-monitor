984ee153b30104a6d0d7a5fc2d48b0eb53ce82e9
sourabh <sourabh@sumologic.com>
2021-06-17 16:02:08 +0530

    first check-in for nginx plus ingress.

---
+++ monitor_packages/nginx-plus-ingress/README.md
@@ -0,0 +1,18 @@
+# sumologic-nginx-plus-ingress-monitors
+
+This Terraform script configures pre-packaged Sumo Logic Monitors for Nginx-Plus-Ingress using Terraform modules.
+For installation and configuration, please look at Sumo Logic Nginx Plus Ingress [Help Document](https://help.sumologic.com/07Sumo-Logic-Apps/24Web_Servers/Nginx_Ingress_ULM/Install_the_Nginx_Ingress_App_and_View_the_Dashboards).
+
+## License
+
+The Nginx-Ingress Terraform script is licensed under the apache v2.0 license.
+
+## Issues
+
+Raise issues at [Issues](https://github.com/SumoLogic/terraform-sumologic-sumo-logic-monitor/issues)
+
+## Contributing
+
+* Fork the project on [Github](https://github.com/SumoLogic/terraform-sumologic-sumo-logic-monitor).
+* Make your feature addition or fix bug, write tests and commit.
+* Create a pull request with one of the maintainer as Reviewer.
---
+++ monitor_packages/nginx-plus-ingress/nginxplusingress.tf
@@ -0,0 +1,210 @@
+# Sumo Logic Nginx Plus Ingress Metric Monitor
+module "DroppedConnections" {
+  source                    = "SumoLogic/sumo-logic-monitor/sumologic"
+  #version                  = "{revision}"
+  monitor_name                = "Nginx Plus Ingress - Dropped Connections"
+  monitor_description         = "This alert fires when we detect dropped connections for a given Nginx Plus server."
+  monitor_monitor_type        = "Metrics"
+  monitor_parent_id           = sumologic_monitor_folder.tf_monitor_folder.id
+  monitor_is_disabled         = var.monitors_disabled
+
+  # Queries - Multiple queries allowed for Metrics monitor
+  queries = {
+    A = "_origin=kubernetes metric = nginx_ingress_nginx_connections_handled Cluster = * Deployment = * Namespace = * Pod=* | sum by cluster, deployment, namespace, pod"
+    B = "_origin=kubernetes metric = nginx_ingress_nginx_connections_accepted Cluster = * Deployment = * Namespace = * Pod=* | sum by cluster, deployment, namespace, pod"
+    C = "#B - #A along cluster, deployment, namespace, pod"
+  }
+
+  # Triggers
+  triggers = [
+              {
+                  threshold_type = "GreaterThan",
+                  threshold = 0,
+                  time_range = "5m",
+                  occurrence_type = "Always" # Options: Always, AtLeastOnce and MissingData for Metrics
+                  trigger_source = "AnyTimeSeries" # Options: AllTimeSeries and AnyTimeSeries for Metrics. 'AnyTimeSeries' is the only valid triggerSource for 'Critical' trigger
+                  trigger_type = "Critical",
+                  detection_method = "StaticCondition"
+                },
+                {
+                  threshold_type = "LessThanOrEqual",
+                  threshold = 0,
+                  time_range = "5m",
+                  occurrence_type = "Always" # Options: Always, AtLeastOnce and MissingData for Metrics
+                  trigger_source = "AnyTimeSeries" # Options: AllTimeSeries and AnyTimeSeries for Metrics. 'AnyTimeSeries' is the only valid triggerSource for 'Critical' trigger
+                  trigger_type = "ResolvedCritical",
+                  detection_method = "StaticCondition"
+                }
+            ]
+
+  # Notifications
+  group_notifications       = var.group_notifications
+  connection_notifications  = var.connection_notifications
+  email_notifications       = var.email_notifications
+}
+
+# Sumo Logic Nginx Plus Ingress Logs Monitor
+module "AccessFromHighlyMaliciousSource" {
+  source                    = "SumoLogic/sumo-logic-monitor/sumologic"
+  #version                  = "{revision}"
+  monitor_name                = "Nginx Plus Ingress - Access from Highly Malicious Sources"
+  monitor_description         = "This alert fires when an Nginx is accessed from highly malicious IP addresses."
+  monitor_monitor_type        = "Logs"
+  monitor_parent_id           = sumologic_monitor_folder.tf_monitor_folder.id
+  monitor_is_disabled         = var.monitors_disabled
+
+  queries = {
+    A = "${var.logs_data_source} \n| json auto maxdepth 1 nodrop\n| if (isEmpty(log), _raw, log) as nginx_log_message\n| _sourceHost as Server\n| parse regex field=nginx_log_message \"(?<ClientIp>\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3})\"\n| where ClientIp != \"0.0.0.0\" and ClientIp != \"127.0.0.1\"\n| count as ip_count by ClientIp, Server\n| lookup type, actor, raw, threatlevel as MaliciousConfidence from sumo://threat/cs on threat=ClientIp \n| json field=raw \"labels[*].name\" as LabelName \n| replace(LabelName, \"\\\\/\",\"->\") as LabelName\n| replace(LabelName, \"\\\"\",\" \") as LabelName\n| where type=\"ip_address\" and MaliciousConfidence=\"high\"\n| if (isEmpty(actor), \"Unassigned\", actor) as Actor\n| sum (ip_count) as ThreatCount by ClientIp, Server, MaliciousConfidence, Actor, LabelName"
+  }
+
+  # Triggers
+  triggers = [
+              {
+                  threshold_type = "GreaterThan",
+                  threshold = 0,
+                  time_range = "5m",
+                  occurrence_type = "ResultCount"
+                  trigger_source = "AllResults"
+                  trigger_type = "Critical",
+                  detection_method = "StaticCondition"
+                },
+                {
+                  threshold_type = "LessThanOrEqual",
+                  threshold = 0,
+                  time_range = "5m",
+                  occurrence_type = "ResultCount"
+                  trigger_source = "AllResults"
+                  trigger_type = "ResolvedCritical",
+                  detection_method = "StaticCondition"
+                }
+            ]
+
+  # Notifications
+  group_notifications       = var.group_notifications
+  connection_notifications  = var.connection_notifications
+  email_notifications       = var.email_notifications
+}
+
+module "CriticalErrorMessage" {
+  source                    = "SumoLogic/sumo-logic-monitor/sumologic"
+  #version                  = "{revision}"
+  monitor_name                = "Nginx Plus Ingress - Critical Error Messages"
+  monitor_description         = "This alert fires when we detect critical error messages for a given Nginx Plus server."
+  monitor_monitor_type        = "Logs"
+  monitor_parent_id           = sumologic_monitor_folder.tf_monitor_folder.id
+  monitor_is_disabled         = var.monitors_disabled
+
+  queries = {
+    A = "${var.logs_data_source} \n| json auto maxdepth 1 nodrop\n| if (isEmpty(log), _raw, log) as nginx_log_message\n| _sourceHost as Server\n| parse regex field=nginx_log_message \"\\s\\[(?<LogLevel>\\S+)\\]\\s\\d+#\\d+:\\s(?:\\*\\d+\\s|)(?<Message>[A-Za-z][^,]+)(?:,|$)\"\n| where LogLevel in (\"emerg\", \"alert\", \"crit\")\n| formatDate(_messageTime, \"MMM/dd/yyyy HH:mm:ss:SSS Z\") as MessageDate\n| count by MessageDate, Server, LogLevel, Message\n| fields MessageDate, Server, LogLevel, Message"
+  }
+
+  # Triggers
+  triggers = [
+              {
+                  threshold_type = "GreaterThan",
+                  threshold = 0,
+                  time_range = "5m",
+                  occurrence_type = "ResultCount"
+                  trigger_source = "AllResults"
+                  trigger_type = "Critical",
+                  detection_method = "StaticCondition"
+                },
+                {
+                  threshold_type = "LessThanOrEqual",
+                  threshold = 0,
+                  time_range = "5m",
+                  occurrence_type = "ResultCount"
+                  trigger_source = "AllResults"
+                  trigger_type = "ResolvedCritical",
+                  detection_method = "StaticCondition"
+                }
+            ]
+
+  # Notifications
+  group_notifications       = var.group_notifications
+  connection_notifications  = var.connection_notifications
+  email_notifications       = var.email_notifications
+}
+
+module "HighClientError" {
+  source                    = "SumoLogic/sumo-logic-monitor/sumologic"
+  #version                  = "{revision}"
+  monitor_name                = "Nginx Plus Ingress - High Client (HTTP 4xx) Error Rate"
+  monitor_description         = "This alert fires when there are too many HTTP requests (>5%) with a response status of 4xx."
+  monitor_monitor_type        = "Logs"
+  monitor_parent_id           = sumologic_monitor_folder.tf_monitor_folder.id
+  monitor_is_disabled         = var.monitors_disabled
+
+  queries = {
+    A = "${var.logs_data_source} \n| json auto maxdepth 1 nodrop\n| if (isEmpty(log), _raw, log) as nginx_log_message\n| _sourceHost as Server\n| parse regex field=nginx_log_message \"(?<Method>[A-Z]+)\\s(?<URL>\\S+)\\sHTTP/[\\d\\.]+\\\"\\s(?<StatusCode>\\d+)\\s(?<Size>[\\d-]+)\\s\\\"(?<Referrer>.*?)\\\"\\s\\\"(?<UserAgent>.+?)\\\".*\"\n| if (StatusCode matches \"4*\", 1, 0) as ServerError\n| sum(ServerError) as ServerErrors, count as TotalRequests by Server\n| (ServerErrors/TotalRequests) * 100 as ErrorPercentage\n| where ErrorPercentage > 5\n| fields Server, ErrorPercentage, ServerErrors, TotalRequests"
+  }
+
+  # Triggers
+  triggers = [
+              {
+                  threshold_type = "GreaterThan",
+                  threshold = 0,
+                  time_range = "5m",
+                  occurrence_type = "ResultCount"
+                  trigger_source = "AllResults"
+                  trigger_type = "Critical",
+                  detection_method = "StaticCondition"
+                },
+                {
+                  threshold_type = "LessThanOrEqual",
+                  threshold = 0,
+                  time_range = "5m",
+                  occurrence_type = "ResultCount" # Options: Always, AtLeastOnce and MissingData for Metrics
+                  trigger_source = "AllResults" # Options: AllTimeSeries and AnyTimeSeries for Metrics. 'AnyTimeSeries' is the only valid triggerSource for 'Critical' trigger
+                  trigger_type = "ResolvedCritical",
+                  detection_method = "StaticCondition"
+                }
+            ]
+
+  # Notifications
+  group_notifications       = var.group_notifications
+  connection_notifications  = var.connection_notifications
+  email_notifications       = var.email_notifications
+}
+
+module "HighServerError" {
+  source                    = "SumoLogic/sumo-logic-monitor/sumologic"
+  #version                  = "{revision}"
+  monitor_name                = "Nginx Plus Ingress - High Server (HTTP 5xx) Error Rate"
+  monitor_description         = "This alert fires when there are too many HTTP requests (>5%) with a response status of 5xx."
+  monitor_monitor_type        = "Logs"
+  monitor_parent_id           = sumologic_monitor_folder.tf_monitor_folder.id
+  monitor_is_disabled         = var.monitors_disabled
+
+  # Queries - Multiple queries allowed for Metrics monitor
+  queries = {
+    A = "${var.logs_data_source} \n| json auto maxdepth 1 nodrop\n| if (isEmpty(log), _raw, log) as nginx_log_message\n| _sourceHost as Server\n| parse regex field=nginx_log_message \"(?<Method>[A-Z]+)\\s(?<URL>\\S+)\\sHTTP/[\\d\\.]+\\\"\\s(?<StatusCode>\\d+)\\s(?<Size>[\\d-]+)\\s\\\"(?<Referrer>.*?)\\\"\\s\\\"(?<UserAgent>.+?)\\\".*\"\n| if (StatusCode matches \"5*\", 1, 0) as ServerError\n| sum(ServerError) as ServerErrors, count as TotalRequests by Server\n| (ServerErrors/TotalRequests) * 100 as ErrorPercentage\n| where ErrorPercentage > 5\n| fields Server, ErrorPercentage, ServerErrors, TotalRequests"
+  }
+
+  # Triggers
+  triggers = [
+              {
+                  threshold_type = "GreaterThan",
+                  threshold = 0,
+                  time_range = "5m",
+                  occurrence_type = "ResultCount" # Options: Always, AtLeastOnce and MissingData for Metrics
+                  trigger_source = "AllResults" # Options: AllTimeSeries and AnyTimeSeries for Metrics. 'AnyTimeSeries' is the only valid triggerSource for 'Critical' trigger
+                  trigger_type = "Critical",
+                  detection_method = "StaticCondition"
+                },
+                {
+                  threshold_type = "LessThanOrEqual",
+                  threshold = 0,
+                  time_range = "5m",
+                  occurrence_type = "ResultCount" # Options: Always, AtLeastOnce and MissingData for Metrics
+                  trigger_source = "AllResults" # Options: AllTimeSeries and AnyTimeSeries for Metrics. 'AnyTimeSeries' is the only valid triggerSource for 'Critical' trigger
+                  trigger_type = "ResolvedCritical",
+                  detection_method = "StaticCondition"
+                }
+            ]
+
+  # Notifications
+  group_notifications       = var.group_notifications
+  connection_notifications  = var.connection_notifications
+  email_notifications       = var.email_notifications
+}
